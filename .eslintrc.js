efebc039888aa9a3746154ccc423ac7e60a399fe
longhuqiang <longhuqiang@aspirecn.com>
2021-10-27 21:29:25 +0800

    提交项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
     },
-    extends: [
-        'plugin:vue/essential',
-        '@vue/standard'
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
     ],
-    parserOptions: {
-        parser: 'babel-eslint'
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'indent': ["off", 2]
-    }
-}
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {}
+};
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引用vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 // 加载全局样式文件
 import './styles/index.less'
+//注册使用vant组建库
+Vue.use(Vant);
 Vue.config.productionTip = false
 
 new Vue({
